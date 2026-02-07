f53cd1445972d8d9002d621da359ceef0a224284
yu8mamikamari <mxxayxmay@gmail.com>
2026-02-07 20:38:30 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord
+import os
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+client.run(os.getenv('TOKEN'))
